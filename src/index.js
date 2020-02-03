31ac38493ee24142da59e0a5616b2157ed8131d2
Luke Edwards <luke.edwards05@gmail.com>
2020-02-03 14:47:33 -0800

    fix: allow `pause()` within loop

--- src/index.js
+++ src/index.js
@@ -1,12 +1,12 @@
 export default function (draw, fps) {
 	var delay = 1e3 / (fps || 60);
-	var tmp, pid, last, frame=-1;
+	var tmp, pid, last, frame = -1;
 
 	function loop(time) {
 		if (!last) last = time;
 		tmp = ((time - last) / delay) | 0;
-		if (pid && tmp > frame) draw(frame = tmp);
-		pid = requestAnimationFrame(loop);
+		if (pid && tmp > frame) draw(frame=tmp);
+		if (pid) pid = requestAnimationFrame(loop);
 	}
 
 	return {
@@ -15,9 +15,8 @@ export default function (draw, fps) {
 		},
 		pause: function () {
 			if (pid) {
-				cancelAnimationFrame(pid);
+				last = pid = cancelAnimationFrame(pid);
 				frame = -1;
-				last = 0;
 			}
 		}
 	};
